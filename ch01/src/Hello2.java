7001052c268054c7d9057652c990b580ae69c8c6
imbel9830 <imbel9830@gmail.com>
2022-12-16 16:38:53 +0900

    자바연습

---
+++ ch01/src/Hello2.java
@@ -0,0 +1,25 @@
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl + shift + L 단축키 전체 목록
+		// ctrl + or - 확대 축소
+		// ctrl + D 한 줄 삭제
+		// ctrl + alt + down 아래복사 
+		// alt + sheft + A 선택 수정
+		// alt + up or down 행단위 이동
+		// ctrl + i 자동 들여쓰기
+		// ctrl + / 주석처리
+		// ctrl + space 자동완성
+		System.out.println("hello World");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+
+}
